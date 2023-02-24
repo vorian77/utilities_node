f74fc5c0be165bd185aa82ce280d7564796bed4a
Phyllip Hall <phall776@gmail.com>
2023-02-24 14:42:56 -0500

    refactor: prep for multiple utilities

---
+++ http.js
@@ -0,0 +1,25 @@
+const axios = require('axios'); 
+const qs = require('qs'); 
+
+module.exports = async function (method, url, queryParms) {
+  let options;
+
+  switch(method.toLowerCase()) {
+    case 'get':
+      options = { method, url, params: queryParms, timeout: 1000 }
+      break;
+
+    default:  // put, post, delete, patch
+      options = { method, url, data: qs.stringify(queryParms), timeout: 1000 }
+  }
+    
+  try {
+      console.log('Axios options...', options);
+      const response = await axios(options);
+      console.log('Axios successful.');
+      return response
+    } catch(err) {
+      err.status = err.response.status;
+      throw err;
+    }
+}
--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-const axios = require('axios'); 
-const qs = require('qs'); 
+const http = require('./http.js');
 
-module.exports = async function (method, url, queryParms) {
-  let options;
-
-  switch(method.toLowerCase()) {
-    case 'get':
-      options = { method, url, params: queryParms, timeout: 1000 }
-      break;
-
-    default:  // put, post, delete, patch
-      options = { method, url, data: qs.stringify(queryParms), timeout: 1000 }
-  }
-    
-  try {
-      console.log('Axios options...', options);
-      const response = await axios(options);
-      console.log('Axios successful.');
-      return response
-    } catch(err) {
-      err.status = err.response.status;
-      throw err;
-    }
-}
+module.exports = { http }
